3c62e54512626e097c679b4b55f1e010793e8c98
Mohamed AbdulkadirAbdulahi <mohamedabdulkadirabdulahi@Mohameds-MacBook-Air.local>
2026-02-27 18:15:05 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,36 @@
-import Image from "next/image";
+import React from 'react'
+import Link from 'next/link'
+import { FaGraduationCap } from "react-icons/fa";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className='text-black'>
+     <div className='flex flex-col justify-center items-center mt-60 border  mx-auto p-15 w-[400px] h-180 rounded-lg shadow-sm border-gray-200 bg-white '>
+<div className='flex flex-col justify-center items-center pb-10'>
+  <FaGraduationCap className="text-blue-500" size={60} />
+      <h1 className='text-3xl font-bold text-gray-800 mb-4'>Welcome back!</h1>
+      <p className="text-gray-500 text-center">Enter your credentials to access your account.</p>
+     </div>
+
+     <input type="text" placeholder="Username" className="border border-gray-300 rounded px-2 py-2 mb-4 w-full"/>
+     <input type="password" placeholder="Password" className="border border-gray-300 rounded px-2 py-2 mb-4 w-full"/>
+
+     <Link href="/" className=' block w-full text-center bg-black py-2 px-3 text-white rounded-lg font-bold transition-transform duration-300 hover:bg-gray-800 hover:scale-105'>
+Sing In
+     </Link>
+<div className='pb-3'>
+     <p className='text-center border-b border-gray-300 pb-4 pt-3'> Don't have an account? <Link href="/register"className='text-blue-500'>Register here</Link></p>
+     <p className='pt-2 text-center'>Demo Accounts (password:password123)</p>
+</div>
+     <div className='flex flex-col space-y-2 justify-center items-center pb-10'>
+    <p className='w-full block text-center border-1 text-center font-semibold text-black border border-gray-400 p-2 rounded-sm hover:border-blue-500 '>Admin Demo</p>
+    <p className='w-full block text-center border-1 text-center font-semibold text-black border border-gray-400 p-2 rounded-sm hover:border-blue-500 '>Teacher Demo</p>
+    <p className='w-full block text-center border-1 text-center font-semibold text-black border border-gray-400 p-2 rounded-sm hover:border-blue-500 '>Student Demo</p>
+   
+     </div>
     </div>
-  );
+    </div>
+  )
 }
+
+export default page
