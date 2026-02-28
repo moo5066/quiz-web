a2c81df2c2e2ef888adc7c5d5a05ab8ddd638ac2
Mohamed AbdulkadirAbdulahi <mohamedabdulkadirabdulahi@Mohameds-MacBook-Air.local>
2026-02-28 23:34:23 +0300

    update

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -30,47 +30,55 @@ const page = () => {
        </div>
       </nav> 
 
-      <div className='max-w-xl p-4 mx-auto'>
+      <div className='max-w-6xl mx-auto p-15 '>
         <div className='mb-6'>
-        <h1 className='font-bold text-2xl'>Student Dashboard</h1>
+        <h1 className='font-bold text-2xl'>Student Dashboard</h1> 
         <p className='text-gray-400'>Take quizzes and track your progress</p>
         </div>
-
-        <div className='w-50 h-50 border flex justify-between items-center p-4 rounded-lg shadow-sm border-gray-300 bg-white text-black'>
+<div className='flex flex-col justify-center items-center gap-6  sm:flex-row sm:justify-between items-center w-full'>
+        <div className='w-full h-20 border flex justify-between items-center p-12 rounded-lg shadow-sm border-gray-300 bg-white text-black'>
             <div className='flex flex-col'>
                 <p className='text-gray-600'>Quizzes Taken</p>
                 <h1 className='font-bold text-3xl'>0</h1>
             </div>
             <div className='bg-blue-300 p-2 rounded-full text-white'>
             <IoMdCheckmarkCircleOutline size={30} className='text-blue-800'/>
-</div></div>
-
-        <div>
-            <div>
-                <p>Total Score</p>
-                <h1>0</h1>
-            </div>
-            <TfiCup/>
+</div>
 </div>
 
-        <div>
-            <div>
-                <p>Available Quizzes</p>
-                <h1>5</h1>
+        <div className='w-full h-20 border flex justify-between items-center p-12 rounded-lg shadow-sm border-gray-300 bg-white text-black'>
+            <div  >
+                <p className='text-gray-600'>Total Score</p>
+                <h1 className='font-bold text-3xl'>0</h1>
             </div>
-            <FaBookOpen/>
+            <div className='bg-yellow-300 p-2 rounded-full text-white'>
+            <TfiCup size={30} className='text-yellow-800'/>
+</div>
 </div>
 
-        <div>
-            <div>
-                <p>Subjects</p>
-                <h1>5</h1>
+        <div className='w-full h-20 border flex justify-between items-center p-12 rounded-lg shadow-sm border-gray-300 bg-white text-black'>
+            <div className='flex flex-col'>
+                <p className='text-gray-600'>Available Quizzes</p>
+                <h1 className='font-bold text-3xl'>5</h1>
             </div>
-            <MdOutlineWatchLater/>
+            <div className='bg-green-300 p-3 rounded-full'>
+            <FaBookOpen size={30} className='text-blue-600'/>
+            </div>
+</div>
+
+        <div className='w-full h-20 border flex justify-between items-center p-12 rounded-lg shadow-sm border-gray-300 bg-white text-black'>
+            <div className='flex flex-col'>
+                <p className='text-gray-600'>Subjects</p>
+                <h1 className='font-bold text-3xl'>5</h1>
+            </div>
+            <div className='bg-purple-300 p-2 rounded-full text-white'>
+            <MdOutlineWatchLater size={30} className='text-purple-800'/>
+</div>
 </div>
 
 
         
+      </div>
       </div>
     </div>
   )
