2300c8fac1b5f6e58e947af35135a35bcc7f5996
Mohamed AbdulkadirAbdulahi <maxamedgoley@gmail.com>
2026-03-05 11:31:12 +0300

    update

--- app/sceince/page.jsx
+++ app/sceince/page.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { GiOpenBook } from "react-icons/gi";
-import Link from 'next/link';
 import { IoMdArrowRoundBack } from "react-icons/io";
+import Link from 'next/link';
 
 const page = () => {
   return (
-    <div className='text-black mt-100'>
-                <Link href="/dashboard" className='absolute left-0 top-0 p-5 text-black'><IoMdArrowRoundBack size={20}/></Link>
+    <div className='text-black flex justify-around mt-50'>
+        <Link href="/dashboard" className='absolute left-0 top-0 p-5 text-black'><IoMdArrowRoundBack size={20}/></Link>
 
        <div className='border border-gray-300 w-[400px] h-60 sm:h-50 p-3  rounded-lg mx-5 '>
           <div className='flex justify-between space-x-10'>
               <GiOpenBook  size={40} className='bg-blue-200 p-2 text-blue-500 rounded-lg'/>
               <p className='border  flex justify-center items-center p-0.5 border-gray-300 rounded-lg text-sm'>30s/question</p>
           </div>
-      
+
           <div className='py-4'>
               <h1 className='text-xl font-bold'>Physics 101</h1>
-              <p className='text-gray-500'>Sceince</p>
+              <p className='text-gray-500'>Science</p>
+          </div>
+          <Link href="/sceince/physics-quiz" className='py-2 block w-[90%]  mx-auto bg-black text-white p-1 text-center rounded-lg font-semibold' >Start Quiz</Link>
+        </div>
+
+        <div className='border border-gray-300 w-[400px] h-60 sm:h-50 p-3 rounded-md rounded-lg mx-5 '>
+          <div className='flex justify-between space-x-10'>
+              <GiOpenBook  size={40} className='bg-blue-200 p-2 text-blue-500 rounded-lg'/>
+              <p className='border  flex justify-center items-center p-0.5 border-gray-300 rounded-lg text-sm'>40s/question</p>
+          </div>
+
+          <div className='py-4'>
+              <h1 className='text-xl font-bold'>Biology Basics</h1>
+              <p className='text-gray-500'>Science</p>
           </div>
           <Link href="#" className='py-2 block w-[90%]  mx-auto bg-black text-white p-1 text-center rounded-lg font-semibold' >Start Quiz</Link>
         </div>
